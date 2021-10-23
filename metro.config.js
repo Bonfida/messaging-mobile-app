367b4baf7f08f10e20878337940c04e6b737ec3e
dr497 <anand.ratiney@gmail.com>
2021-10-23 22:50:28 +0800

    Fix compilation issue

--- metro.config.js
+++ metro.config.js
@@ -1,8 +1,14 @@
-module.exports = {
-  resolver: {
-    sourceExts: ["jsx", "js", "ts", "tsx", "cjs"],
-    extraNodeModules: {
-      stream: require.resolve("readable-stream"),
-    },
+const { getDefaultConfig } = require("expo/metro-config");
+
+const config = getDefaultConfig(__dirname);
+
+config.transformer.minifierPath = "metro-minify-terser";
+
+config.resolver = {
+  sourceExts: ["jsx", "js", "ts", "tsx", "cjs"],
+  extraNodeModules: {
+    stream: require.resolve("readable-stream"),
   },
 };
+
+module.exports = config;
