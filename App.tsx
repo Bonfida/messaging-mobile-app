f1e21a4c52a21a6e6d0d59c2b8c50532d8354ec0
DR497 <47689875+dr497@users.noreply.github.com>
2022-04-19 23:08:29 +0800

    v1

--- App.tsx
+++ App.tsx
@@ -25,6 +25,8 @@ import GalleryScreen from "./screens/GalleryScreen";
 import "./global";
 import "react-native-gesture-handler";
 import "react-native-url-polyfill/auto";
+import "react-native-get-random-values";
+import { v4 as uuidv4 } from "uuid";
 
 import { Feather } from "@expo/vector-icons";
 
